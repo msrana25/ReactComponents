a75a7b14f6b8b49172391a11e0c31b2c70214647
msrana25 <mnprtrana61@gmail.com>
2023-03-25 22:06:35 -0400

    navigation in-depth

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import Dropdown from "./components/Dropdown";
-import { useState } from "react";
+import Link from "./components/Link"
+import Route from "./components/Route"
+import AccordionPage from "./pages/AccordionPage";
+import DropdownPage from "./pages/DropdownPage";
 
 function App() {
-    const [selection, setSelection] = useState(null);
-
-    const handleSelect = (option) => {
-        setSelection(option);
-    }
-
-    const options = [
-        { label: 'Red', value: 'red' },
-        { label: 'Green', value: 'green' },
-        { label: 'Blue', value: 'blue' }
-
-    ]
-    return <Dropdown options={options} selection={selection} onSelect={handleSelect} />;
-
+  return (
+    <div>
+      <Link to="/accordion">Go to accordion</Link>
+      <Link to="/dropdown">Go to dropdown</Link>
+    <div>
+      <Route path="/accordion">
+        <AccordionPage />
+      </Route>
+      <Route path="/dropdown">
+        <DropdownPage />
+      </Route>
+    </div>
+    </div>
+  )
 }
 
-export default App;
+export default App
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,32 @@
-import { useState } from "react"
+import { useEffect, useState,useRef } from "react";
+import { GoChevronDown } from "react-icons/go"
+import Panel from "./Panel";
 
-function Dropdown({ options, selection, onSelect }) {
+function Dropdown({ options, value, onChange }) {
     const [isOpen, setIsOpen] = useState(false);
+    const divEl = useRef();
+
+    useEffect(() => {
+        const handler = (event) => {
+           
+            if(!divEl.current)
+            {
+                return;
+            }
+           
+            if(!divEl.current.contains(event.target)){
+            setIsOpen(false);
+
+           }
+        };
+
+        document.addEventListener('click',handler,true);
+
+        return () => {
+            document.removeEventListener('click', handler);
+        }
+
+    },[]);
 
     const handleClick = () => {
         setIsOpen((current) => { return !current });
@@ -9,12 +34,13 @@ function Dropdown({ options, selection, onSelect }) {
 
     const handleOptionClick = (option) => {
         setIsOpen(false);
-        onSelect(option);
+        onChange(option);
     }
 
 
+
     const renderedOptions = options.map((option) => {
-        return <div onClick={() => handleOptionClick(option)} key={option.value}>
+        return <div className="hover:bg-sky-100 rounded cursor-pointer p-1" onClick={() => handleOptionClick(option)} key={option.value}>
             {option.label}
         </div>
 
@@ -22,11 +48,16 @@ function Dropdown({ options, selection, onSelect }) {
 
 
     return (
-        <div>
-            <div onClick={handleClick}>
-                {selection?.label || 'Select ...'}
-            </div>
-            {isOpen && <div>{renderedOptions}</div>}
+        <div ref = {divEl} className="w-48 relative">
+            <Panel
+                className="flex justify-between items-center cursor-pointer"
+                onClick={handleClick}>
+                {value?.label || 'Select ...'}
+                <GoChevronDown className="text-lg" />
+            </Panel>
+            {isOpen && <Panel
+                className="absolute top-full">{renderedOptions}
+            </Panel>}
         </div>
     )
 }
